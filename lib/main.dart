d5e02b3b94883d977930a42c5fb1cd5cb01bd08e
twesigetrejan <trejantwesige19@gmail.com>
2026-02-06 12:24:05 +0300

    styles: add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,20 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  List names = ["Kira", "Kampala", "Mubende"];
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+
+  final screens = [
+    Center(child: Text("Home")),
+    Center(child: Text("Search")),
+    Center(child: Text("Profile")),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,47 +36,23 @@ class MyApp extends StatelessWidget {
             IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app)),
           ],
         ),
-        body: ListView.builder(
-          itemCount: names.length,
-          itemBuilder: (context, index) => ListTile(title: Text(names[index])),
-        ),
 
-        // body: Center(
-        //   child: SizedBox(
-        //     height: 900,
-        //     child: Image.asset(
-        //       'assets/icons/welcome.webp',
-        //       fit: BoxFit.fitHeight,
-        //     ),
-        //   ),
-        // ),
-        // body: ListView(
-        //   scrollDirection: Axis.horizontal,
-        //   children: [
-        //     SizedBox(height: 20),
-        //     Expanded(
-        //       child: Container(
-        //         color: Colors.green,
-        //         padding: EdgeInsets.all(20),
-        //         height: 400,
-        //         width: 400,
-        //       ),
-        //     ),
-        //     Expanded(
-        //       child: Container(
-        //         color: Colors.green,
-        //         padding: EdgeInsets.all(20),
-        //         height: 400,
-        //       ),
-        //     ),
-        //     Expanded(
-        //       child: Container(
-        //         color: Colors.green,
-        //         padding: EdgeInsets.all(20),
-        //       ),
-        //     ),
-        //   ],
-        // ),
+        // <-- Body changes with selectedIndex
+        body: screens[selectedIndex],
+
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: selectedIndex,
+          onDestinationSelected: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+            NavigationDestination(icon: Icon(Icons.search), label: "Search"),
+            NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+          ],
+        ),
       ),
     );
   }
